40706b7e8f6507994c097557fd69212fe54b8d8e
jessegong37110 <86636228+jessegong37110@users.noreply.github.com>
2021-10-15 13:34:09 +1300

    catch error

--- pages/location/[magic].tsx
+++ pages/location/[magic].tsx
@@ -5,96 +5,100 @@ import fetch from "node-fetch";
 // @ts-nocheck
 //@ts-ignore
 function Location({ data }) {
-  const router = useRouter();
-  // const { magicValue } = router.query;
-  const { locationsData } = data;
-  const [searchValue, setSearchValue] = useState(data.magicValue);
-  const [locations, setLocations] = useState(locationsData);
-  // console.log(locations);
-  // console.log(data);
-  // console.log(router.query);
-  // console.log(magic);
+  try {
+    const router = useRouter();
+    // const { magicValue } = router.query;
+    const { locationsData } = data;
+    const [searchValue, setSearchValue] = useState(data.magicValue);
+    const [locations, setLocations] = useState(locationsData);
+    // console.log(locations);
+    // console.log(data);
+    // console.log(router.query);
+    // console.log(magic);
 
-  // Render data...
+    // Render data...
 
-  const handleSearchValueChange = (e: any) => {
-    setSearchValue(e.target.value);
-  };
+    const handleSearchValueChange = (e: any) => {
+      setSearchValue(e.target.value);
+    };
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    const res = await fetch(
-      `https://exposure-events.develop.tracing.tmp19.net/current-exposure-locations.json`
-    );
-    const locations = await res.json();
-    //@ts-ignore
-    setLocations(locations.items);
-    console.log(locations);
-  };
-  if (data.error) {
-    return <div>{data.error}</div>;
-  } else {
-    return (
-      <div>
-        <h2>Location page</h2>
-        <a href="/">Go home</a>
-        <ul>
-          <li>
-            <Link href="/location/abc">
-              <a>Go to /location/abc</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/location/abc?foo=bar">
-              <a>Also goes to /location/abc?foo=bar</a>
-            </Link>
-          </li>
-          <li>
-            <a
-              href="/location/abcde"
-              onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-              }}
-            >
-              Do nothing
-            </a>
-          </li>
-        </ul>
-        <form action="/location/ddd" method="GET" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="region"
-            value={searchValue}
-            onChange={handleSearchValueChange}
-          />
-          <input type="submit" name="search" />
-        </form>
+    const handleSubmit = async (e: any) => {
+      e.preventDefault();
+      const res = await fetch(
+        `https://exposure-events.develop.tracing.tmp19.net/current-exposure-locations.json`
+      );
+      const locations = await res.json();
+      //@ts-ignore
+      setLocations(locations.items);
+      console.log(locations);
+    };
+    if (data.error) {
+      return <div>{data.error}</div>;
+    } else {
+      return (
+        <div>
+          <h2>Location page</h2>
+          <a href="/">Go home</a>
+          <ul>
+            <li>
+              <Link href="/location/abc">
+                <a>Go to /location/abc</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/location/abc?foo=bar">
+                <a>Also goes to /location/abc?foo=bar</a>
+              </Link>
+            </li>
+            <li>
+              <a
+                href="/location/abcde"
+                onClick={(e) => {
+                  e.preventDefault();
+                  e.stopPropagation();
+                }}
+              >
+                Do nothing
+              </a>
+            </li>
+          </ul>
+          <form action="/location/ddd" method="GET" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="region"
+              value={searchValue}
+              onChange={handleSearchValueChange}
+            />
+            <input type="submit" name="search" />
+          </form>
 
-        <table>
-          <tbody>
-            {
-              //@ts-ignore
-              locations &&
-                locations.map((location: any, index: number) => {
-                  return (
-                    <tr key={index}>
-                      <td>{location.eventId}</td>
-                      <td>{location.eventName}</td>
-                      <td>{location.startDateTime}</td>
-                      <td>{location.endDateTime}</td>
-                    </tr>
-                  );
-                })
-            }
-          </tbody>
-        </table>
+          <table>
+            <tbody>
+              {
+                //@ts-ignore
+                locations &&
+                  locations.map((location: any, index: number) => {
+                    return (
+                      <tr key={index}>
+                        <td>{location.eventId}</td>
+                        <td>{location.eventName}</td>
+                        <td>{location.startDateTime}</td>
+                        <td>{location.endDateTime}</td>
+                      </tr>
+                    );
+                  })
+              }
+            </tbody>
+          </table>
 
-        <noscript>
-          <span>Your js is disabled</span>
-        </noscript>
-      </div>
-    );
+          <noscript>
+            <span>Your js is disabled</span>
+          </noscript>
+        </div>
+      );
+    }
+  } catch (e: any) {
+    return <div>{e.message}</div>;
   }
 }
 
